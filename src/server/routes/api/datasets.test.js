90bbb7cd6846073cda2e104a87927a600c558b27
shreshth <shreshthsrivastava2@gmail.com>
2020-10-07 10:41:03 -0400

    wrote template.html test

---
+++ src/server/routes/api/datasets.test.js
@@ -0,0 +1,15 @@
+// let request = require('supertest');
+
+// const server = 'http://localhost:3000';
+// request = request(server);
+
+// describe('Route integration', () => {
+//   describe('/', () => {
+//     it('sends back html file', () => {
+//       request
+//         .get('/')
+//         .expect('Content-Type', /text\/html/)
+//         .expect(200);
+//     });
+//   });
+// });
