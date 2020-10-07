241e49fa0d3d4a81cd2afd689798c9c904251957
shreshth <shreshthsrivastava2@gmail.com>
2020-10-06 20:04:01 -0400

    working on setting up jest config

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+// module.exports = {
+//   clearMocks: true,
+//   collectCoverageFrom: ['src/**/*.{js, jsx, mjs}'],
+//   coverageDirectory: 'coverage',
+//   moduleFileExtensions: ['js', 'json', 'jsx'],
+//   setupFiles: [], // when adding enzyme tests, make sure to edit this
+//   testEnvironment: 'jsdom',
+// };
+
+module.exports = {
+  testEnvironment: 'node',
+};
