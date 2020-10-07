cc76c541240c92a153f56157919498b18b49319d
Andrew <andrew.lovato@gmail.com>
2020-10-07 10:48:58 -0400

    making sure this works

--- src/server/routes/api/auth.test.js
+++ src/server/routes/api/auth.test.js
@@ -1,15 +1,15 @@
-// let request = require('supertest');
+let request = require('supertest');
 
-// const server = 'http://localhost:3000';
-// request = request(server);
+const server = 'http://localhost:3000';
+request = request(server);
 
-// describe('Route integration', () => {
-//   describe('/', () => {
-//     it('sends back html file', () => {
-//       request
-//         .get('/')
-//         .expect('Content-Type', /text\/html/)
-//         .expect(200);
-//     });
-//   });
-// });
+describe('Route integration', () => {
+  describe('/', () => {
+    it('sends back html file', () => {
+      request
+        .get('/')
+        .expect('Content-Type', /text\/html/)
+        .expect(200);
+    });
+  });
+});
